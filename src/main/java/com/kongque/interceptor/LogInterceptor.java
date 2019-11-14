d3b8b62690c86eb5ff50822690d0326eefe1e176
wanshan <602926338@qq.com>
2019-11-14 17:32:11 +0800

    拦截器

---
+++ src/main/java/com/kongque/interceptor/LogInterceptor.java
@@ -0,0 +1,12 @@
+package com.kongque.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LogInterceptor  implements HandlerInterceptor{
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    
+
+}
